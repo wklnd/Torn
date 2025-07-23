257f4a1a9dc42c3132ecc5b07b7ede1b00d6ed22
Oscar Wiklund <147804906+wklnd@users.noreply.github.com>
2025-07-23 08:21:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use('/api/player', playerRoutes);
 app.use(express.static('public'));
 
 // Start the server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; // Change port?
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
