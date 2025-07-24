a7d6afafba24e4f99294aad567da9480c783d633
Oscar Wiklund <147804906+wklnd@users.noreply.github.com>
2025-07-24 15:58:36 +0200

    Update index.js
    refactor.

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
+
 const targetRoutes = require('./routes/target');
 const playerRoutes = require('./routes/player');
+
 require('./database/mongo');
-
 require('./jobs/fetchStatsJobs');
-
 require('./jobs/fetchTarget');
 
 const app = express();
 
-app.use(bodyParser.json());
+// Middleware
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Use the target routes
+// Routes
 app.use('/api/target', targetRoutes);
 app.use('/api/player', playerRoutes);
 
-app.use(express.static('public'));
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal server error' });
+});
 
-// Start the server
-const PORT = process.env.PORT || 3000; // Change port?
+// Start server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
